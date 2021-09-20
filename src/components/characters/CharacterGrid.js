9b9b38d2c8640199a7424e3a8f1f89b712fa0dce
Sawan Kumar <88134243+SawanKumar007@users.noreply.github.com>
2021-09-20 19:37:20 +0530

    Update CharacterGrid.js

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CharacterItem from './CharacterItem'
 import Spinner from '../ui/Spinner'
 
+
+
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
     <Spinner />
